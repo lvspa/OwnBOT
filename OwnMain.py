bc5145ca27ab5b8c8be736eaab75a3075e88cbff
Alexandre Vitor <166564473+lvspa@users.noreply.github.com>
2024-07-01 21:39:51 -0300

    Update OwnMain.py

--- OwnMain.py
+++ OwnMain.py
@@ -4,7 +4,8 @@ import time
 
 # Lista de URLs permitidas
 white_list = [
-#É possivel adicionar qualquer URL que vocẽ precisar, alguns exemplos: 
+
+#Alguns URLs de exemplo: 
     
     "https://www.pichau.com.br/",
     "https://www.kabum.com.br/",
@@ -16,7 +17,11 @@ white_list = [
     "https://store.steampowered.com/",
     "https://www.magazineluiza.com.br/",
     "https://www.youtube.com/"
-
+    "https://www.terabyteshop.com.br/"
+    "https://patoloco.com.br"
+    "https://www.americanas.com.br/"
+    "https://www.amazon.com.br/"
+    "https://www.nuuvem.com/br-pt/"
 ]
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -30,7 +35,7 @@ reddit = praw.Reddit(
     user_agent=''
 )
 
-subreddit = reddit.subreddit('')
+subreddit = reddit.subreddit('computadores')
 
 while True:
     try:
@@ -48,10 +53,25 @@ while True:
                     found = True
                     break
 
+            for submission in subreddit.stream.submissions(skip_existing=True):
+                title_words = submission.title.split()
+                selftext_words = submission.selftext.split()
+                found = False
+
+                for word in title_words + selftext_words:
+                    if word.startswith('http') and not any(word.startswith(allowed_url) for allowed_url in white_list):
+                        bot_comment = submission.reply("uuh uuh ATENÇÃO! [Removido pelo bot de supervisão]")
+                        logger.info(f"Respondido ao post: {submission.id}")
+                        submission.delete()
+                        logger.info(f"Post excluído: {submission.id} por violação de regras.")
+                        submission.mod.lock()
+                        found = True
+                        break
+
             if found:
                 break
 
-            time.sleep(1)
+            time.sleep(2)
 
     except Exception as e:
         logger.error(f"Ocorreu um erro: {e}")
